202cf055be570a5e2fd1c68a562e33b23ba08044
volosgoto <volosgoto@yahoo.com>
2018-04-23 11:41:07 +0300

    creade init config

--- config/init.php
+++ config/init.php
@@ -1,4 +1,39 @@
 <?php
 
+// Development mode
+const DEBUG = 1;
 
-echo 'Hello init';
+// Production mode hide all errors
+//const DEBUG = 0;
+
+define("ROOT", dirname(__DIR__));
+
+define("WWW", ROOT . '/public');
+
+define("APP", ROOT . '/app');
+
+define("CORE", ROOT . '/vendor/ishop/core');
+
+define("LIBS", ROOT . '/vendor/ishop/libs');
+
+define("CACHE", ROOT . '/tmp/cache');
+
+define("CONF", ROOT . '/config');
+
+define("LAYOUT", ROOT . 'default');
+
+
+// http://hostname.loc/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+// http://hostname.loc/public/
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+// http://hostname.loc
+echo $app_path = str_replace('/public/', '', $app_path);
+
+define("PATH", $app_path);
+
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/composer/autoload_psr4.php';
