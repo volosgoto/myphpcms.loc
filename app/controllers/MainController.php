7a5ecb6e0ea29d02798040a02353ba511aa2e7ff
volosgoto <volosgoto@yahoo.com>
2018-04-27 14:43:31 +0300

    add Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,12 +3,20 @@
 namespace app\controllers;
 
 use ishop\App;
+use ishop\Cache;
 
 class MainController extends AppController {
 
     public function indexAction(){
         $posts = \R::findAll('test');
-        //debug($posts);
+
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrey', 'Jane', 'Mike'];
+
+        $cache = Cache::instance();
+//        $cache->set('test', $names);
+//        $cache->delete('test');
 
         $this->setMeta(App::$app->getProperty('site_name'), 'главная', 'главная');
         $this->set(compact('posts'));
